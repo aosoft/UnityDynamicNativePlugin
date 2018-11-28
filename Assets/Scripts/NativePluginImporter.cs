0ff6842bd1579169e6e643aa68f1493de4dd655c
aosoft <tan-y@aosoft.jp>
2018-11-28 13:55:13 +0900

    add MainPlugin project

---
+++ Assets/Scripts/NativePluginImporter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class NativePluginImporter : MonoBehaviour
+{
+	[DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+	private static extern System.IntPtr LoadLibrary(string lpFileName);
+
+	[DllImport("kernel32.dll", SetLastError = true)]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	private static extern bool FreeLibrary(System.IntPtr hModule);
+
+	[DllImport("kernel32", CharSet = CharSet.Ansi, SetLastError = true)]
+	private static extern System.IntPtr GetProcAddress(System.IntPtr hModule, string procName);
+
+	delegate int FnSum(int a, int b);
+
+	private System.IntPtr _dll = System.IntPtr.Zero;
+
+	private void Awake()
+	{
+		_dll = LoadLibrary(@"Assets/Plugins/x86_64/MainPlugin.dll");
+		var p = GetProcAddress(_dll, "Sum");
+		var funcSum = Marshal.GetDelegateForFunctionPointer<FnSum>(p);
+
+		Debug.Log(funcSum(10, 20));
+
+		DontDestroyOnLoad(this);
+	}
+
+	private void OnDestroy()
+	{
+		if (_dll != System.IntPtr.Zero)
+		{
+			FreeLibrary(_dll);
+			_dll = System.IntPtr.Zero;
+		}
+	}
+}
---
+++ NativePluginProject/MainPlugin/MainPlugin.cpp
@@ -0,0 +1,31 @@
+﻿// MainPlugin.cpp : DLL アプリケーション用にエクスポートされる関数を定義します。
+//
+
+#include "stdafx.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+IUnityInterfaces *g_unityInterfaces = nullptr;
+
+int32_t UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API Sum(int32_t a, int32_t b)
+{
+	return a + b;
+}
+
+void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces* unityInterfaces)
+{
+	g_unityInterfaces = unityInterfaces;
+}
+
+void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload()
+{
+	g_unityInterfaces = nullptr;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+
